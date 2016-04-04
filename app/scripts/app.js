d174cb98c7fae8f70241bf165b2e63d28ad2c8ed
Carlos Cañado <ccanado@gmail.com>
2016-04-04 21:49:16 +0200

    Use the original iron-swipeable-pages by MeTaNoV and wrap it inside krypton-view to add local functionality

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,10 +2,9 @@
   'use strict';
 
   var app = document.querySelector('#app');
-  app.route = 'Organic Green Lemonade';
-
-  // Sets app default base URL
+  app.route = '';
   app.baseUrl = '/';
+
   if (window.location.port === '') {  // if production
     // Uncomment app.baseURL below and
     // set app.baseURL to '/your-pathname/' if running from folder in production
@@ -14,12 +13,11 @@
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
+  app.addEventListener('dom-change', function () {
     console.log('Our app is ready to rock!');
   });
 
-  window.addEventListener('WebComponentsReady', function() {
+  window.addEventListener('WebComponentsReady', function () {
     // imports are loaded and elements have been registered
   });
-
 })(document);
